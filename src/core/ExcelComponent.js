f30d218cc414413358eaf132e80bb67554276ba5
Vladislav <umorphine@gmail.com>
2022-08-11 20:59:06 +0400

    [Dev] Улучшение фреймворка

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,16 +5,25 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners);
     this.name = options.name || ''
 		this.emitter = options.emitter
+		this.unsubscribers = []
 
 		this.prepare()
   }
 
 	prepare() {}
 
-  // Возвращает шаблон компонента
   toHTML() {
     return ''
   }
+
+	$emit(event, ...args) {
+		this.emitter.emit(event, ...args)
+	}
+
+	$on(event, fn) {
+		const unsub = this.emitter.subscribe(event, fn)
+		this.unsubscribers.push(unsub)
+	}
 
   init() {
     this.initDOMListeners()
@@ -22,5 +31,6 @@ export class ExcelComponent extends DomListener {
 
   destroy() {
     this.removeDOMListeners()
+		this.unsubscribers.forEach((unsub) => unsub())
   }
 }
