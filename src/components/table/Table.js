313f444dfe4d03ddd0672932ff583f064c5e3411
Vladislav <umorphine@gmail.com>
2022-08-13 21:38:34 +0400

    [Dev] Связывание Store и UI

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -37,6 +37,10 @@ export class Table extends ExcelComponent {
 		this.$on('formula:done', () => {
 			this.selection.current.focus()
 		})
+
+		this.$subscribe((state) => {
+			console.log('TableState', state)
+		})
 	}
 
 	selectCell($cell) {
@@ -54,7 +58,7 @@ export class Table extends ExcelComponent {
 					.map((id) => this.$root.find(`[data-id="${id}"]`))
 				this.selection.selectGroup($cells)
 			} else {
-				this.selection.select($target)
+				this.selectCell($target)
 			}
 		}
 	}
