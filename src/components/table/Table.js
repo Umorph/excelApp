8e6c69b1a582af2616e29575ce9dc9b66a969edd
Vladislav <umorphine@gmail.com>
2022-08-13 19:03:29 +0400

    [Dev] Создание интерактива

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -11,7 +11,7 @@ export class Table extends ExcelComponent {
 	constructor($root, options) {
 		super($root, {
 			name: 'Table',
-			listeners: ['mousedown', 'keydown'],
+			listeners: ['mousedown', 'keydown', 'input'],
 			...options,
 		})
 	}
@@ -28,11 +28,20 @@ export class Table extends ExcelComponent {
 		super.init()
 
 		const $cell = this.$root.find('[data-id="0:0"]')
-		this.selection.select($cell)
+		this.selectCell($cell)
 
 		this.$on('formula:input', (text) => {
 				this.selection.current.text(text)
 		})
+
+		this.$on('formula:done', () => {
+			this.selection.current.focus()
+		})
+	}
+
+	selectCell($cell) {
+		this.selection.select($cell)
+		this.$emit('table:select', $cell)
 	}
 
 	onMousedown(event) {
@@ -65,7 +74,11 @@ export class Table extends ExcelComponent {
 			event.preventDefault()
 			const id = this.selection.current.id(true)
 			const $next = this.$root.find(nextSelector(key, id))
-			this.selection.select($next)
+			this.selectCell($next)
 		}
+	}
+
+	onInput(event) {
+		this.$emit('table:input', $(event.target))
 	}
 }
